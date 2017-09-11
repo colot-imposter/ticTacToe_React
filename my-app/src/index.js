b1b4193385e8dff89cf2fc3bf5cd892f6b30c835
Colton Dowling <Colton@cd3style.com>
2017-09-11 15:09:56 -0500

    added click function to button

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -5,16 +5,19 @@ import './index.css';
 class Square extends React.Component {
   render() {
     return (
-      <button className="square">
-        {/* TODO */}
+      <button className="square" onClick={() => alert('click')}>
+         {this.props.value}
       </button>
     );
   }
 }
 
 class Board extends React.Component {
+
+
+
   renderSquare(i) {
-    return <Square />;
+    return <Square value={i}/>;
   }
 
   render() {
